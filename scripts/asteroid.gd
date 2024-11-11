8dfcfa3fc66560be2ce0f62d62ec752bef826236
Zax71 <67716263+zax71@users.noreply.github.com>
2024-11-11 17:36:21 +0000

    We have many asteroids

--- scripts/asteroid.gd
+++ scripts/asteroid.gd
@@ -2,10 +2,14 @@ extends RigidBody2D
 
 @onready var sprite = $AnimatedSprite2D
 
+var rng = RandomNumberGenerator.new()
+func _ready():
+	self.set_position(Vector2(rng.randi_range(-200, 200), rng.randi_range(-200, 200)))
+
 func _on_body_entered(body):
 	if body.name == "Earth":
 		sprite.play("Explode")
 		await sprite.animation_finished
-		## await get_tree().create_timer(0.5).timeout
-		self.queue_free()
-
+		var new_asteroid = self.duplicate()
+		get_parent().add_child(new_asteroid)
+		new_asteroid.set_position(Vector2(rng.randi_range(-200, 200), rng.randi_range(-200, 200)))
